ab08cf1550a0e919cd523fc7ecd6fd8ac0455f16
Dang Thanh <thanhdd.it@gmail.com>
2016-03-02 09:38:04 +0700

    feat(optimize css)

--- js/load-styles.js
+++ js/load-styles.js
@@ -4,22 +4,12 @@ var cb = function () {
 	var l1 = document.createElement('link');
 	l1.rel = 'stylesheet';
 	l1.href = 'https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic';
-	head_ele.parentNode.insertBefore(l1, head_ele);
+	head_ele.appendChild(l1);
 
 	var l2 = document.createElement('link');
 	l2.rel = 'stylesheet';
 	l2.href = 'https://fonts.googleapis.com/css?family=Arizonia';
-	head_ele.parentNode.insertBefore(l2, head_ele);
-
-	var l3 = document.createElement('link');
-	l3.rel = 'stylesheet';
-	l3.href = '/css/style.css';
-	head_ele.parentNode.insertBefore(l3, head_ele);
-
-	var l4 = document.createElement('link');
-	l4.rel = 'stylesheet';
-	l4.href = '/css/syntax.css';
-	head_ele.parentNode.insertBefore(l4, head_ele);
+	head_ele.appendChild(l2);
 };
 var raf = requestAnimationFrame || mozRequestAnimationFrame ||
 	webkitRequestAnimationFrame || msRequestAnimationFrame;
